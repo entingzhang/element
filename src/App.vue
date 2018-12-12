e55be347354a47dbba0aef7a0ba00ee408a72197
entingzhang <591692578@qq.com>
2018-12-12 16:18:49 +0800

    这个是element项目 创建了布局 Aside的建立

---
+++ src/App.vue
@@ -0,0 +1,58 @@
+<template>
+	<div id="app">
+		<header></header>
+		<aside>
+		<aside-side></aside-side>
+		</aside>
+		<main>
+			<router-view/>
+		</main>
+	</div>
+</template>
+
+<script>
+	//import HelloWorld from '@/components/HelloWorld.vue'
+	import AsideSide from "@/components/AsideSide.vue"
+	export default {
+  name: 'APP',
+  components: {
+  	AsideSide
+  }
+}
+</script>
+<style scoped="scoped">
+	html,
+	body,
+	#app {
+		width: 100%;
+		height: 100%;
+		margin: 0;
+	}
+	
+	header {
+		width: 100%;
+		height: 50px;
+		position: fixed;
+		top: 0px;
+		left: 0px;
+		border-bottom: 1px solid #42B983;
+	}
+	
+	aside {
+		width: 220px;
+		height: 100%;
+		position: fixed;
+		top: 0px;
+		left: 0px;
+		background-color: #42B983;
+		color: #fff;
+	}
+	
+	main {
+		position: fixed;
+		top: 50px;
+		right: 0px;
+		bottom: 0px;
+		left: 220px;
+	}
+</style>
---
+++ src/components/AsideSide.vue
@@ -0,0 +1,81 @@
+<template>
+	<el-menu default-active="0" class="el-menu-vertical-demo" @open="open" @close="close" @select="select" background-color="#42B983" text-color="#fff" active-text-color="#303133">
+		<el-submenu :index="i+''" v-for="(item,i) in nav">
+			<template slot="title">
+				<i :class="item.icon"></i>
+				<span v-text="item.title"></span>
+			</template>
+			<el-menu-item-group>
+				<template slot="title"></template>
+				<el-menu-item :index="sub.path" v-for="(sub,j) in nav[i].sub" v-text="sub.title"></el-menu-item>
+			</el-menu-item-group>
+		</el-submenu>
+	</el-menu>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				nav: [{
+					title: 'basic',
+					icon: 'el-icon-location',
+					sub: [{
+						title: 'Layout 布局',
+						path: 'Layout'
+					}, {
+						title: 'Container 布局容器',
+						path: 'Container'
+					}, {
+						title: 'Color 色彩',
+						path: 'Color'
+					}]
+				}, {
+					title: 'form',
+					icon: 'el-icon-menu',
+					sub: [{
+							title: 'Radio 单选框',
+							path: 'Radio'
+						},
+						{
+							title: 'Checkbox 多选框',
+							path: 'Checkbox'
+						}, {
+							title: 'Input 输入框',
+							path: 'Input'
+						}]
+				}, {
+					title: 'data',
+					icon: 'el-icon-document',
+					sub: [{
+						title: 'Table 表格',
+						path: 'Table'
+					}, {
+						title: 'Tag 标签',
+						path: 'Tag'
+					}, {
+						title: 'Progress 进度条',
+						path: 'Progress'
+					}]
+				}]
+			}
+		},
+		methods: {
+			open(key, keyPath) {
+				console.log(key, keyPath)
+			},
+			close(key, keyPath) {
+				console.log(key, keyPath)
+			},
+			select(index, indexPath) {
+				this.$router.push(index);
+				console.log(index);
+				console.log(indexPath);
+			}
+		}
+	}
+</script>
+
+<style>
+
+</style>
