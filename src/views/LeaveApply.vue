be2216bc509c022aadd9091df3942d8901d2f925
entingzhang <591692578@qq.com>
2018-12-17 14:34:50 +0800

    用数据库的方法建立了请假单的提交和请假单审核

--- src/views/LeaveApply.vue
+++ src/views/LeaveApply.vue
@@ -4,13 +4,19 @@
 			<span>LeaveApply请假申请</span>
 			<el-button style="float: right; padding: 3px 0" type="text">操作按钮</el-button>
 		</div>
-		<el-table :data="apply" style="width: 100%">
+		<el-table :data="apply" style="width: 100%" v-loading="loading">
 			<el-table-column type="index" width="50"></el-table-column>
-			<el-table-column prop="name" width="120" label="姓名"></el-table-column>
-			<el-table-column prop="departmet" width="120" label="部门"></el-table-column>
-			<el-table-column prop="because" label="请假事由"></el-table-column>
-			<el-table-column prop="starttime" label="开始时间"></el-table-column>
-			<el-table-column prop="endtime" label="结束时间"></el-table-column>
+			<el-table-column prop="name" width="80" label="姓名"></el-table-column>
+			<el-table-column prop="partment" width="80" label="部门"></el-table-column>
+			<el-table-column prop="region" label="事由"></el-table-column>
+			<el-table-column label="时间">
+				<template slot-scope="scope">
+					{{timeFrmate(scope.row.start)}}至{{timeFrmate(scope.row.end)}}
+				</template>
+			</el-table-column>
+			<el-table-column prop="other" label="紧急联系人"></el-table-column>
+			<el-table-column prop="other_phone" width="80" label="联系人电话"></el-table-column>
+			<el-table-column prop="other_phone" label="备注"></el-table-column>
 			<el-table-column label="操作">
 				<template slot-scope="scope">
 					<el-button :plain="true" size="mini" @click="open">同意</el-button>
@@ -25,44 +31,35 @@
 </template>
 
 <script>
+	import axios from "axios";
+	import qs from "qs";
 	export default {
 		data() {
 			return {
 				size: 10,
-				total: 100,
+				total: 0,
 				page: 1,
-				apply: [{
-					id: '1',
-					name: '郭德纲',
-					departmet:'财务部',
-					because: '事假',
-					starttime: '2018-12-28',
-					endtime: '2018-12-30'
-				}, {
-					id: '1',
-					name: '于谦',
-					departmet:'财务部',
-					because: '事假',
-					starttime: '2018-12-28',
-					endtime: '2018-12-30'
-				}, {
-					id: '1',
-					name: '岳云鹏',
-					departmet:'财务部',
-					because: '事假',
-					starttime: '2018-12-28',
-					endtime: '2018-12-30'
-				}, {
-					id: '1',
-					name: '张云雷',
-					departmet:'财务部',
-					because: '事假',
-					starttime: '2018-12-28',
-					endtime: '2018-12-30'
-				}]
+				apply: [],
+				loading:false,
+				current: 1
 			}
 		},
 		methods: {
+			timeFrmate(timestamp){
+				let time=new Date(timestamp*1000);
+				let y=time.getFullYear();
+				let M=time.getMonth();
+				let d=time.getDate();
+				let h=time.getHours();
+				let m=time.getMinutes();
+				let s=time.getSeconds();
+				
+				h=h<10?('0'+h):h;
+				m=m<10?('0'+m):m;
+				s=s<10?('0'+s):s;
+				return y+'-'+(M+1)+'-'+d
+//				+''+h+':'+m+':'+s
+			},
 			open() {
 				this.$message({type: 'success',message:'您已同意请假申请'});
 			},
@@ -71,32 +68,31 @@
 				this.$message({type: 'error',message:'您已驳回请假申请'});
 			},
 			handleSizeChange(s) {
-				this.apply = this.getApplyList(s);
+				this.size=s;
+				this.page=1;
+				this.getApplyList(1);
 			},
 			handleCurrentChange(p) {
-				this.apply = this.getApplyList(this.size);
+				this.page=p;
+				this.getApplyList(p);
 			},
-			getApplyList(n) {
-				let arr = [];
-				let names = ['郭德纲', '于谦', '岳云鹏', '张云雷', '郭麒麟', '孙越', '孟鹤堂', '高晓攀', '谢金'];
-				let departmets=['财务部','人事部','运营部','销售部','后勤部'];
-				let becauses = ['事假', '病假', '婚假', '丧假', '其他'];
-				for(let i = 0; i < n; i++) {
-					let index = Math.floor(Math.random() * names.length);
-					let indexa = Math.floor(Math.random() * departmets.length);
-					let indexb = Math.floor(Math.random() * becauses.length);
-					let s = {
-						id: 1,
-						name: names[index],
-						departmet:departmets[indexa],
-						because: becauses[indexb],
-						starttime: "2019"+"-"+Math.ceil(Math.random() * 12)+"-"+Math.ceil(Math.random() * 30),
-						endtime: "2019"+"-"+Math.ceil(Math.random() * 12)+"-"+Math.ceil(Math.random() * 30),
-					};
-					arr.push(s);
-				}
-				return arr;
+			getApplyList(page) {
+				this.loading=true;
+				const url = "http://192.168.255.30:8888/index.php/Index/Index/getLeaveList";
+				axios.post(url, qs.stringify({
+					page:page,
+					size:this.size,
+				})).then( res=>{
+					this.apply=res.data.list;
+					this.total=res.data.total;
+					this.loading=false;
+				}).catch( err=>{
+					this.loading=false;
+				});
 			}
+		},
+		created(){
+			this.getApplyList(1);
 		}
 	}
 </script>
--- src/views/QingJia.vue
+++ src/views/QingJia.vue
@@ -3,143 +3,148 @@
 		<div slot="header" class="clearfix">
 			<h3><i class="fa fa-list"></i> 请假单</h3>
 		</div>
-		<span>姓名：</span>
-		<el-input placeholder="请输入姓名" suffix-icon="el-icon-edit" blur v-model="input2"></el-input>
-		<br />
-		<span>部门：</span>
-		<el-select v-model="value" clearable placeholder="请选择部门">
-			<el-option v-for="item in options" :key="item.value" :label="item.label" :value="item.value">
-			</el-option>
-		</el-select>
-		<br />
-		<span>事由：</span>
-		<el-select v-model="value1" clearable placeholder="请假事由">
-			<el-option v-for="item in tings" :key="item.val" :label="item.label" :value="item.val">
-			</el-option>
-		</el-select>
-		<div class="block">
-			<span class="demonstration">请假开始时间：</span>
-			<el-date-picker v-model="value2" align="right" type="date" placeholder="选择日期" :picker-options="pickerOptions1">
-			</el-date-picker>
-			<span class="demonstration2">请假开始时间：</span>
-			<el-date-picker v-model="value3" align="right" type="date" placeholder="选择日期" :picker-options="pickerOptions1">
-			</el-date-picker>
-		</div>
-		<br />
-		<el-input type="textarea" :autosize="{ minRows: 4, maxRows: 4}" placeholder="备注：请说明原因" v-model="textarea3">
-		</el-input>
-		<div class="block">
-			<span>紧急联系人姓名：</span>
-			<el-input placeholder="请输入紧急联系人姓名" suffix-icon="el-icon-edit" blur v-model="input2"></el-input>
-			<br />
-			<span>紧急联系人电话：</span>
-			<el-input placeholder="联络方式" suffix-icon="el-icon-mobile-phone" blur v-model="input2"></el-input>
-		</div>
-		<el-button type="text" @click="open4">提交请假申请</el-button>
+		<el-form :model="options" :rules="rules" ref="options" label-width="100px" class="demo-ruleForm">
+			<el-row :gutter="10">
+				<el-col :span="12">
+					<el-form-item label="请假人:" prop="name">
+						<el-input v-model="options.name"></el-input>
+					</el-form-item>
+				</el-col>
+				<el-col :span="12">
+					<el-form-item label="事由:" prop="region">
+						<el-select v-model="options.region" placeholder="请选择请假原因" style="width:100%">
+							<el-option label="事假" value="事假"></el-option>
+							<el-option label="婚假" value="婚假"></el-option>
+							<el-option label="病假" value="病假"></el-option>
+							<el-option label="年假" value="年假"></el-option>
+							<el-option label="其他" value="其他"></el-option>
+						</el-select>
+					</el-form-item>
+				</el-col>
+			</el-row>
+			<el-row :gutter="10">
+				<el-col :span="12">
+					<el-form-item label="部门:" prop="partment">
+						<el-select v-model="options.partment" placeholder="请选择部门" style="width:100%">
+							<el-option label="技术部" value="技术部"></el-option>
+							<el-option label="财务部" value="财务部"></el-option>
+							<el-option label="后勤部" value="后勤部"></el-option>
+							<el-option label="人事部" value="人事部"></el-option>
+							<el-option label="运营部" value="运营部"></el-option>
+						</el-select>
+					</el-form-item>
+				</el-col>
+				<el-col :span="12">
+					<el-form-item label="时间:">
+						<el-date-picker v-model="time" style="width:100%" value-format="timestamp" type="datetimerange" range-separator="至" start-placeholder="开始日期" end-placeholder="结束日期" align="right">
+						</el-date-picker>
+					</el-form-item>
+				</el-col>
+			</el-row>
+			<el-row :gutter="10">
+				<el-col :span="12">
+					<el-form-item label="紧急联系人:" prop="other">
+						<el-input v-model="options.other"></el-input>
+					</el-form-item>
+				</el-col>
+				<el-col :span="12">
+					<el-form-item label="联系人电话:" prop="other">
+						<el-input v-model="options.other_phone"></el-input>
+					</el-form-item>
+				</el-col>
+			</el-row>
+			<el-form-item label="备注:" prop="leave_desc">
+				<el-input v-model="options.leave_desc">请假备注说明</el-input>
+			</el-form-item>
+			<el-form-item>
+				<el-button type="primary" @click="submitForm('options')" :loading="loading">提交</el-button>
+				<el-button @click="resetForm('options')">重置</el-button>
+			</el-form-item>
+		</el-form>
 	</el-card>
 </template>
 
 <script>
+	import axios from "axios";
+	import qs from "qs";
 	export default {
 		data() {
 			return {
-				value: '',
-				value1: '',
-				value2: '',
-				value3: '',
-				textarea3: '',
-
-				options: [{
-					value: '选项1',
-					label: '财务部'
-				}, {
-					value: '选项2',
-					label: '人事部'
-				}, {
-					value: '选项3',
-					label: '运营部'
-				}, {
-					value: '选项4',
-					label: '销售部'
-				}, {
-					value: '选项5',
-					label: '后勤部'
-				}],
-				tings: [{
-					val: '选项6',
-					label: '事假'
-				}, {
-					val: '选项7',
-					label: '病假'
-				}, {
-					val: '选项8',
-					label: '丧假'
-				}, {
-					val: '选项9',
-					label: '婚假'
-				}, {
-					val: '选项10',
-					label: '其他'
-				}],
-				pickerOptions1: {
-					shortcuts: [{
-						text: '今天',
-						onClick(picker) {
-							picker.$emit('pick', new Date());
+				time:[],
+				options: {
+					name: '',
+					partment: '',
+					region: '',
+					start: '',
+					end: '',
+					leave_desc: '',
+					other: '',
+					other_phone: ''
+				},
+				loading: false,
+				rules: {
+					name: [{
+							required: true,
+							message: '请输入请假人',
+							trigger: 'blur'
+						},
+						{
+							min: 2,
+							max: 10,
+							message: '长度在 2 到 10 个字符',
+							trigger: 'blur'
 						}
-					}, {
-						text: '昨天',
-						onClick(picker) {
-							const date = new Date();
-							date.setTime(date.getTime() - 3600 * 1000 * 24);
-							picker.$emit('pick', date);
-						}
-					}, {
-						text: '一周前',
-						onClick(picker) {
-							const date = new Date();
-							date.setTime(date.getTime() - 3600 * 1000 * 24 * 7);
-							picker.$emit('pick', date);
-						}
-					}]
+					]
 				}
+
 			}
 		},
 		methods: {
-			open4() {
-				const h = this.$createElement;
-				this.$msgbox({
-					title: '请假申请',
-					message: h('p', null, [
-						h('span', null, '请问是否提交请假申请 '),
-					]),
-					showCancelButton: true,
-					confirmButtonText: '确定',
-					cancelButtonText: '取消',
-					beforeClose: (action, instance, done) => {
-						if(action === 'confirm') {
-							instance.confirmButtonLoading = true;
-							instance.confirmButtonText = '提交中...';
-							setTimeout(() => {
-								done();
-								setTimeout(() => {
-									instance.confirmButtonLoading = false;
-								}, 300);
-							}, 3000);
-						} else {
-							done();
-						}
+			addLeave() {
+				this.loading = true;
+				let url = "http://192.168.255.30:8888/index.php/Index/Index/addLeave";
+				this.options.start = this.time[0] / 1000;
+				this.options.end = this.time[1] / 1000;
+				axios.post(url, qs.stringify({
+					data: this.options
+				})).then(res => {
+					if(res.data.code > 0) {
+						this.$notify({
+							title: '成功',
+							message: res.data.msg,
+							type: 'success'
+						});
+					} else {
+						this.$notify({
+							title: '失败',
+							message: res.data.msg,
+							type: 'error'
+						});
 					}
-				}).then(action => {
-					this.$message({
-						type: 'success',
-						message: '申请提交成功！'
+					this.loading = false;
+				}).catch(err => {
+					this.$notify({
+						title: '错误',
+						message: '服务器错误',
+						type: 'error'
 					});
+					this.loading = false;
 				});
+			},
+			submitForm(formName) {
+				this.$refs[formName].validate((valid) => {
+					if(valid) {
+						console.log("通过验证");
+						this.addLeave();
+					} else {
+						console.log('error submit!!');
+						return false;
+					}
+				});
+			},
+			resetForm(formName) {
+				this.$refs[formName].resetFields();
 			}
-			//			handleInput(e) {
-			//				this.val = e.target.value.replace(/[^\d]/g, '');
-			//			}
 		}
 	}
 </script>
@@ -147,19 +152,5 @@
 <style scoped="scoped">
 	.clearfix {
 		text-align: center;
-	}
-	
-	.el-input,
-	.el-select {
-		max-width: 25%;
-		margin: 20px 0px;
-	}
-	
-	.el-input.is-focus {
-		border: 1px solid #42B983 !important;
-	}
-	
-	.demonstration2 {
-		margin-left: 20px;
 	}
 </style>
